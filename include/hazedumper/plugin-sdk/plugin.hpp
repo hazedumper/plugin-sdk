c1660550badbf29c973c063a39a069fb3febcdf7
mikilian <michael.kilian2406@gmail.com>
2022-06-26 19:18:12 +0200

    🏗️ Updated abstraction layer of plugin

--- include/hazedumper/plugin-sdk/plugin.hpp
+++ include/hazedumper/plugin-sdk/plugin.hpp
@@ -2,14 +2,14 @@
 
 #include "prerequisites.hpp"
 
-namespace hazedumper {
-ABSTRACT_CLASS(plugin)
+namespace hazedumper::detail {
+ABSTRACT_CLASS(abstract_plugin)
 {
 public:
     using create_plugin_fn = void(*)(plugin_ptr&);
 
     virtual
-    ~plugin() = default;
+    ~abstract_plugin() = default;
 
     NODISCARD
     virtual
@@ -30,6 +30,11 @@ public:
     virtual
     auto
     priority() const noexcept -> i32 = 0;
+
+    NODISCARD
+    virtual
+    auto
+    version() const noexcept -> const detail::version& = 0;
 
     NODISCARD
     virtual
@@ -60,16 +65,73 @@ public:
         const dependencies_ptr& deps
     ) -> void
     {}
+
+    NODISCARD
+    virtual
+    auto
+    sdk_version() const noexcept -> detail::version
+    {
+        return hazed::sdk_version;
+    }
 };
 }
 
+namespace hazedumper {
+ABSTRACT_CLASS(plugin)
+    : public detail::abstract_plugin
+{
+public:
+    explicit 
+    plugin(
+        const detail::version& vers
+    ) noexcept
+        : version_(vers)
+    {}
+
+    plugin(
+        const u32 major,
+        const u32 minor,
+        const u32 patch
+    ) noexcept
+        : version_(major, minor, patch)
+    {}
+
+    NODISCARD
+    auto
+    version() const noexcept -> const detail::version& override
+    {
+        return version_;
+    }
+
+protected:
+    detail::version version_;
+};
+} //namespace hazedumper
+
+#if !defined(MACRO_CONCAT)
+#   if !defined(MACRO_CONCAT_IMPL)
+#       define MACRO_CONCAT_IMPL(a, b) \
+            a##b
+#   endif //MACRO_CONCAT_IMPL
+#   define MACRO_CONCAT(a, b) MACRO_CONCAT_IMPL(a, b)
+#endif //MACRO_CONCAT
+
 #if !defined(HAZEDUMPER_PLUGIN)
-#   define HAZEDUMPER_PLUGIN(PluginClass, ...)                \
-        extern "C"                                            \
-        HAZEDUMPER_EXPORT                                     \
-        auto                                                  \
-        create_plugin(hazed::plugin_ptr& out) -> void         \
-        {                                                     \
-            out = std::make_shared<PluginClass>(__VA_ARGS__); \
+#   define HAZEDUMPER_PLUGIN(PluginClass, ...)                 \
+        extern "C"                                             \
+        HAZEDUMPER_EXPORT                                      \
+        auto                                                   \
+        create_plugin(hazed::plugin_ptr& out) -> void          \
+        {                                                      \
+            static_assert(                                     \
+                std::is_base_of_v<hazed::plugin, PluginClass>, \
+                MACRO_CONCAT("[hazedumper] class ",            \
+                    MACRO_CONCAT(                              \
+                        #PluginClass,                          \
+                        " doesn't implement hazed::plugin"     \
+                    )                                          \
+                )                                              \
+            );                                                 \
+            out = std::make_shared<PluginClass>(__VA_ARGS__);  \
         }
 #endif //HAZEDUMPER_PLUGIN
