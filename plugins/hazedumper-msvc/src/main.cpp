776d0d1e58c065ad7366a5e7b220f543601f3165
mikilian <michael.kilian2406@gmail.com>
2022-06-26 15:58:35 +0200

    🚀 Added plugin entrypoint

---
+++ plugins/hazedumper-msvc/src/main.cpp
@@ -0,0 +1,59 @@
+#include <hazedumper/plugin-sdk/plugin.hpp>
+#include <hazedumper/plugin-sdk/dependencies.hpp>
+#include <process.hpp>
+
+FINAL_CLASS(hazedumper_msvc)
+    : public hazed::plugin
+{
+public:
+    NODISCARD
+    auto
+    name() const noexcept -> std::string_view override
+    {
+        return "hazedumper-msvc";
+    }
+
+    NODISCARD
+    auto
+    author() const noexcept -> std::string_view override
+    {
+        return "Michael K.";
+    }
+
+    NODISCARD
+    auto
+    description() const noexcept -> std::string_view override
+    {
+        return "Remote process abstraction layer for Windows";
+    }
+
+    NODISCARD
+    auto
+    priority() const noexcept -> hazed::i32 override
+    {
+        return 20;
+    }
+
+    NODISCARD
+    auto
+    load() -> bool override
+    {
+        return true;
+    }
+
+    NODISCARD
+    auto
+    unload() -> void override
+    {}
+
+    NODISCARD
+    auto
+    update_dependencies(
+        const hazed::dependencies_ptr& deps
+    ) -> void override
+    {
+        deps->process(std::make_shared<msvc_process>());
+    }
+};
+
+HAZEDUMPER_PLUGIN(hazedumper_msvc);
