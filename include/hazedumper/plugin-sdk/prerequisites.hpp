3ca92be75db0127e93d088a468ec5d700fad5a2c
michaelkilian <michael.kilian2406@gmail.com>
2022-06-30 17:31:20 +0200

    📌 Bumped version to 0.1.6

--- include/hazedumper/plugin-sdk/prerequisites.hpp
+++ include/hazedumper/plugin-sdk/prerequisites.hpp
@@ -161,7 +161,7 @@ hash_string(
     return std::hash<std::string_view>{}(input);
 }
 
-constexpr inline detail::version sdk_version(0, 1, 5);
+constexpr inline detail::version sdk_version(0, 1, 6);
 }
 
 namespace hazed = hazedumper;
